9835f51d80e6150d10c312c51ae1131caa7d384c
syed Hassan Ali <156366767+SyedHassanali24@users.noreply.github.com>
2026-08-05 16:09:34 +0500

    Update app.js

--- attendance-system/javascript/app.js
+++ attendance-system/javascript/app.js
@@ -1,45 +1,13 @@
 /* ==========================================
-   PROJECT STARTED
+   FIREBASE CONNECTION
 ========================================== */
 
-console.log("Sir Syed Hassan Ali Coaching Management System Loaded Successfully.");
+import { db, auth, storage } from "../firebase/firebase-config.js";
 
-/* ==========================================
-   SIMPLE COUNTER ANIMATION
-========================================== */
+console.log("Application Started");
 
-const counters = document.querySelectorAll(".stat-card h3");
+console.log(db);
 
-counters.forEach(counter => {
+console.log(auth);
 
-    const targetText = counter.innerText;
-
-    const target = parseInt(targetText);
-
-    if (isNaN(target)) return;
-
-    let current = 0;
-
-    const increment = Math.ceil(target / 50);
-
-    const updateCounter = () => {
-
-        current += increment;
-
-        if(current >= target){
-
-            counter.innerText = targetText;
-
-        }else{
-
-            counter.innerText = current;
-
-            requestAnimationFrame(updateCounter);
-
-        }
-
-    };
-
-    updateCounter();
-
-});
+console.log(storage);
